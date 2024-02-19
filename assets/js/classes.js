e37e58ff55da2d802c859d488f1f0b3382d7d135
vinicius <viniciusoficialoliveira@gmail.com>
2024-02-19 20:47:15 -0300

    adding modal select players, adding player images & player names

--- assets/js/classes.js
+++ assets/js/classes.js
@@ -9,6 +9,7 @@ class Isagi {
         this.pass = 78;
         this.def = 75;
         this.spd = 77;
+        this.imagePath = 'assets/imgs/isagi.jpg';
     }
 }
 
@@ -21,6 +22,7 @@ class Kaiser {
         this.pass = 79;
         this.def = 74;
         this.spd = 91;
+        this.imagePath = 'assets/imgs/kaiser.jpg';
     }
 }
 
@@ -33,6 +35,7 @@ class Nagi {
         this.pass = 85;
         this.def = 75;
         this.spd = 82;
+        this.imagePath = 'assets/imgs/nagi.jpg';
     }
 }
 
@@ -45,6 +48,7 @@ class Kunigami {
         this.pass = 65;
         this.def = 75;
         this.spd = 85;
+        this.imagePath = 'assets/imgs/kunigami.jpg';
     }
 }
 
@@ -57,7 +61,28 @@ class Stage {
     }
 
     start() {
+        // player img 
+        this.p1El.querySelector('.playerImg').setAttribute('src', `${this.p1.imagePath}`);
+        this.p2El.querySelector('.playerImg').setAttribute('src', `${this.p2.imagePath}`);
+
+        // player names
         this.p1El.querySelector('.playerName').innerHTML = this.p1.name;
         this.p2El.querySelector('.playerName').innerHTML = this.p2.name;
+
+        // p1 stats
+        this.p1El.querySelector('.offense-number').innerHTML = this.p1.atk;
+        this.p1El.querySelector('.shoot-number').innerHTML = this.p1.sht;
+        this.p1El.querySelector('.dribble-number').innerHTML = this.p1.dri;
+        this.p1El.querySelector('.pass-number').innerHTML = this.p1.pass;
+        this.p1El.querySelector('.defense-number').innerHTML = this.p1.def;
+        this.p1El.querySelector('.speed-number').innerHTML = this.p1.spd;
+
+        // p1 stats
+        this.p2El.querySelector('.offense-number').innerHTML = this.p2.atk;
+        this.p2El.querySelector('.shoot-number').innerHTML = this.p2.sht;
+        this.p2El.querySelector('.dribble-number').innerHTML = this.p2.dri;
+        this.p2El.querySelector('.pass-number').innerHTML = this.p2.pass;
+        this.p2El.querySelector('.defense-number').innerHTML = this.p2.def;
+        this.p2El.querySelector('.speed-number').innerHTML = this.p2.spd;
     }
 }
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,32 @@
-// (atk, sht, dri, pass, def, spd)
+let btnEnviar = document.querySelector('.btnEnviar');
+btnEnviar.addEventListener('click', () => {
+    let backdrop = document.querySelector('.backdrop');
+    backdrop.classList.add('dnone');
 
-let p1 = new Isagi();
-let p2 = new Kaiser();
+    let valuep1 = document.querySelector('#selectP1').value;
+    let valuep2 = document.querySelector('#selectP2').value;
 
-let stage = new Stage(
-    p1,
-    p2,
-    document.querySelector('#player1'),
-    document.querySelector('#player2')
-)
+    let p1;
+    let p2;
 
-stage.start();
+    if (valuep1 == 'Isagi Yoichi') {
+        p1 = new Isagi();
+    } else if (valuep1 == 'Nagi Seishiro') {
+        p1 = new Nagi();
+    }
+
+    if (valuep2 == 'Michael Kaiser') {
+        p2 = new Kaiser();
+    } else if (valuep2 == 'Kunigami Rensuke') {
+        p2 = new Kunigami();
+    }
+
+    let stage = new Stage(
+        p1,
+        p2,
+        document.querySelector('#player1'),
+        document.querySelector('#player2')
+    )
+    
+    stage.start();
+})
